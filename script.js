3b3e9e7dca75ae606f33286b1ace456bdd93e7b2
MildaPau <pauzaite.milda@gmail.com>
2022-01-17 20:12:49 +0200

    apdate

--- script.js
+++ script.js
@@ -16,48 +16,43 @@ var number = 0;
 var number1 = 1;
 var number2 = 1;
 
-input.value = 0;
-
+var gameOver = false;
 
 input.addEventListener("change", function(){
 
-    if(number != maxNumber){
         maxNumber.textContent = (`${input.value}`);
         // console.log(maxNumber);
-    } 
+   
 });
 
 
 player1.addEventListener("click", function(){
 
-     if(number1 <= input.value){
+    if(number1 <= input.value){
         count1.textContent = (`${number1++}`); 
         // console.log(number1-1);
         // console.log(input.value);
         // console.log(maxNumber);
         // console.log(count1);
-        
-        if (number1-1 == input.value){
+    } 
+    if (number1-1 == input.value){
         count1.style.color = "green";
         }
-    } 
-   
 })
 
 player2.addEventListener("click", function(){
     if(number2 <= input.value){
         count2.textContent = (`${number2++}`);
-
-         if (number2-1 == input.value){
+    }
+    if (number2-1 == input.value){
         count2.style.color = "green";
         }
-    }
 })
 
 reset.addEventListener("click", function(){
     count1.textContent = (`${number1 = 0}`);
     count2.textContent = (`${number2 = 0}`);
-    input.textContent = (`${input.value = 0}`);
+    input.textContent = (`${input.value = ""}`);
     maxNumber.textContent = (`${input.value}`);
     count1.style.color = "black";
     count2.style.color = "black";
